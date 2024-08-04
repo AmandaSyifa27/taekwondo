04d664e4599e5d14df1347bebb8326b9171c7e7c
beestr <belva.calista04@gmail.com>
2024-08-04 21:09:24 +0700

    last

--- taekwondo.sql
+++ taekwondo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2024 at 02:42 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Aug 04, 2024 at 04:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,17 +48,10 @@ CREATE TABLE `tbl_anggota` (
 --
 
 INSERT INTO `tbl_anggota` (`id_anggota`, `nama_anggota`, `alamat`, `tgl_lahir`, `tempat_lahir`, `jk`, `no_hp`, `berat_badan`, `tinggi_badan`, `pekerjaan`, `id_sabuk`, `tgl_daftar`, `foto`) VALUES
-(3, 'Amanda szzz', ' alamat alamat alamat', '2024-07-02', 'Indramayu', 'Laki-Laki', '0876942233345', 545, 547, 'Nganggurrr', 6, '2024-06-12', '861-Download_premium_png_of_Side_view_of_a_gray_in_3D_about_car_png_side_view__car__convertible_car__car_modern__and_cabriolet_563927-removebg-preview.png'),
-(5, 'Nabila', ' address', '2024-07-04', 'cirebon', 'Perempuan', '4557545', 45, 445, 'mahasiswa', 12, '2024-06-12', '63-Screenshot (300).png'),
-(6, 'Amanda', ' apaya', '2024-07-27', 'cirebon', 'Perempuan', '7645764557', 45, 456, 'IRT', 3, '2024-06-26', '400-8f120301ceaadfa6557e18aacf33a88a.png'),
-(7, 'Belva Calistung', ' alamtas', '2024-08-03', 'Bandung', 'Perempuan', '0876942233347', 456, 456, 'buli', 16, '2024-06-21', '180-Screenshot (295).png'),
-(8, 'Felicia', ' Deps', '2024-07-05', 'Cirebon', 'Perempuan', '0854864258', 20, 100, '-', 3, '2024-05-06', '789-Screenshot (300).png'),
-(9, 'Nida', ' Imy', '2024-07-26', 'Indramayu', 'Perempuan', '086425677', 24, 25, 'iya', 15, '2024-07-11', '127-girl.png'),
-(10, 'Elang', 'Depok ', '2024-07-05', 'Depok', 'Laki-Laki', '4557545', 30, 130, 'Pelajar', 8, '2024-07-25', '969-Screenshot (214).png'),
-(11, 'Ardi', ' Indonesia', '2024-07-27', 'Indramayu', 'Laki-Laki', '7645764557', 45, 456, 'Pelajar', 8, '2024-05-29', '91-DSC01932.JPG'),
-(12, 'Nayla', 'Yogya ', '2024-07-11', 'Indramayu', 'Perempuan', '0876942233347', 60, 154, 'Pelajar', 14, '2024-07-08', '884-fantasy.jpg'),
-(13, 'Giska', 'Tangerang ', '2024-07-03', 'Tangerang', 'Perempuan', '7474574576457', 456, 456, 'pelazar', 10, '2024-07-03', '707-moon.jpg'),
-(14, 'Dina', 'Mana ', '2024-07-04', 'Depok', 'Perempuan', '46577654', 455, 544, 'Pelajar', 10, '2024-07-22', '910-moon2.jpg');
+(3, 'Amanda Sifaul Zanah', 'Indramayu', '2004-11-27', 'Indramayu', 'Perempuan', '0876942233345', 50, 160, 'Mahasiswa', 13, '2024-06-12', '691-20231020037_Amanda Sifaul Zanah__TI.jpg'),
+(5, 'Nabila Hafidz Syabina', 'Cirebon', '2005-10-20', 'cirebon', 'Perempuan', '081364826704', 50, 160, 'Mahasiswa', 12, '2024-06-12', '29-20231020048_NABILA HAFIDZ SYABINA_TEKNIK INFORMATIKA.png.jpg'),
+(7, 'Belva Calista', 'Cirebon', '2004-12-25', 'Cirebon', 'Perempuan', '081954428306', 50, 160, 'Mahasiswa', 10, '2024-06-21', '207-20231020058_BELVA CALISTA_TEKNIK INFORMATIKA.jpg'),
+(14, 'Nethania Emmanuela Rahadian', 'Cirebon', '2005-09-21', 'Cikampek', 'Perempuan', '087848652946', 50, 160, 'Mahasiswa', 11, '2024-07-22', '621-20231020002_Nethania Emmanuela Rahadian_Teknik Informatika.png.png');
 
 -- --------------------------------------------------------
 
@@ -147,7 +140,11 @@ CREATE TABLE `tbl_tagihan` (
 
 INSERT INTO `tbl_tagihan` (`id_tagihan`, `id_anggota`, `tagihan_bulanan`, `tunggakan`, `total_tagihan`) VALUES
 (3, 12, 100000, 50000, 150000),
-(4, 6, 100000, 100000, 200000);
+(4, 6, 100000, 100000, 200000),
+(5, 7, 100000, 200000, 300000),
+(6, 3, 100000, 0, 100000),
+(7, 5, 100000, 100000, 200000),
+(8, 14, 100000, 0, 100000);
 
 -- --------------------------------------------------------
 
@@ -234,7 +231,7 @@ ALTER TABLE `tbl_sabuk`
 -- AUTO_INCREMENT for table `tbl_tagihan`
 --
 ALTER TABLE `tbl_tagihan`
-  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
